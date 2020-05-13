6f77ea90e295913a31572b9524f198b070558162
Dhruv Kumar <dkumarsw@gmail.com>
2020-05-13 20:40:57 +0530

    Fibonacci

---
+++ Recursion/Fibonacci.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define lint long long int
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define mod 1000000007
+#define MAXN 100001
+#define len length()
+#define testcase(t) for(int i=0;i<t;i++)
+#define endl "\n"
+#define space " "
+lint fibonacci(lint n)
+{
+	//Base Case
+	if(n==0 || n==1)
+		return n;
+	return fibonacci(n-1)+fibonacci(n-2);
+}
+void solve()
+{
+	lint t;
+	cin>>t;
+	testcase(t)
+	{
+		lint n,k,q;
+		string s;
+		char c;
+		cin>>n;
+		cout<<fibonacci(n)<<endl;
+	}
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	
+	solve();
+}
